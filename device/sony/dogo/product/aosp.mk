878d117770562d4ddee42aa32e57e116cb05a877
BlackSoon <41862721+BlackSoon@users.noreply.github.com>
2018-08-18 15:02:41 +0500

    Update aosp.mk

--- device/sony/dogo/product/aosp.mk
+++ device/sony/dogo/product/aosp.mk
@@ -1,2 +1,9 @@
 # AOSP specific products
 ifneq ($(BOARD_AOSP_BASED),)
+
+# AOSP properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.rotation_locked=true \
+    ro.storage_manager.enabled=true
+
+endif
